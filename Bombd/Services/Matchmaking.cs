22a9f1f0895385e5f7a099d381515dc8538be402
Aidan <ajr.uiz2048@gmail.com>
2023-11-04 04:10:33 -0400

    Stub matchmaking methods

--- Bombd/Services/Matchmaking.cs
+++ Bombd/Services/Matchmaking.cs
@@ -1,10 +1,24 @@
 ﻿using Bombd.Attributes;
 using Bombd.Core;
+using Bombd.Helpers;
 using Bombd.Protocols;
+using Bombd.Types.Services;
 
 namespace Bombd.Services;
 
 [Service("matchmaking", 10510, ProtocolType.TCP)]
 public class Matchmaking : BombdService
 {
+    [Transaction("beginMatchmaking")]
+    public void BeginMatchmaking(TransactionContext context)
+    {
+        context.Response.MethodName = "matchmakingBegin";
+        context.Response["matchmakingBeginTime"] = TimeHelper.LocalTime.ToString();
+    }
+
+    [Transaction("cancelMatchmaking")]
+    public void CancelMatchmaking(TransactionContext context)
+    {
+        context.Response.MethodName = "matchmakingCanceled";
+    }
 }
