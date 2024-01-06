0c5869b7a55c568c2df67363ba2d7a00bf8cce77
Aidan <ajr.uiz2048@gmail.com>
2024-01-06 10:33:30 -0500

    Use LD_LIBRARY_PATH in Docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Load the OpenSSL binaries we compiled, there's probably a better way to do this
-ldconfig /usr/local/lib/
+LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH
 
 # Make sure we're in the root folder so we can load assets
 cd /bombd/app
